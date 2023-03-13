b7ad46faaa87d1a8d1ffce128c7b7fc49fc82dae
hun1001 <l01022899266@gmail.com>
2023-03-13 11:10:42 +0900

    원석신의 코드 ㄷㄷ

---
+++ Client/ClientProgram.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client;
+
+internal abstract class Program
+{
+    private static async Task Main()
+    {
+        using var clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        var clientEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8080);
+
+        await clientSocket.ConnectAsync(clientEndPoint);
+
+        while (true)
+        {
+            var data = Console.ReadLine();
+            var buffer = Encoding.UTF8.GetBytes(data);
+            await clientSocket.SendAsync(buffer, SocketFlags.None);
+            Console.WriteLine($"Sent: {data}");
+            var receivedBytes = await clientSocket.ReceiveAsync(buffer, SocketFlags.None);
+            data = Encoding.UTF8.GetString(buffer, 0, receivedBytes);
+            Console.WriteLine($"Received: {data}");
+        }
+    }
+}
---
+++ Server/ServerProgram.cs
@@ -0,0 +1,42 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server;
+
+internal abstract class Program
+{
+    private static async Task Main()
+    {
+        using var serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        var serverEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8080);
+        serverSocket.Bind(serverEndPoint);
+        serverSocket.Listen(10);
+        Console.WriteLine("Server is listening on port 8080");
+        while (true)
+        {
+            var clientSocket = await serverSocket.AcceptAsync();
+            Console.WriteLine("Client connected");
+            ThreadPool.QueueUserWorkItem(_ => ReadAsync(clientSocket));
+
+            if (clientSocket.RemoteEndPoint is IPEndPoint remoteIpEndPoint)
+            {
+                Console.WriteLine($"Client IP: {remoteIpEndPoint.Address}, port: {remoteIpEndPoint.Port}");
+            }
+        }
+    }
+
+    private static async void ReadAsync(Socket clientSocket)
+    {
+        while (true)
+        {
+            var buffer = new byte[1024];
+            var receivedBytes = await clientSocket.ReceiveAsync(buffer, SocketFlags.None);
+            var data = Encoding.UTF8.GetString(buffer, 0, receivedBytes);
+            Console.WriteLine($"Received: {data}");
+            clientSocket.Send(buffer, 0, receivedBytes, SocketFlags.None);
+            Console.WriteLine($"Sent: {data}");
+        }
+    }
+}
