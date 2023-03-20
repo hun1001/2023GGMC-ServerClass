ed869c9ec863db9292e514cd7f1c7406d2bb8991
hun1001 <l01022899266@gmail.com>
2023-03-20 09:46:59 +0900

    코드 주석 추가

--- Client/Program.cs
+++ Client/Program.cs
@@ -11,11 +11,12 @@ internal class Program
         }
     }
     
-    
     private static void Main(string[] args)
     {
         Thread t = new Thread(MainThread);
+        // 쓰레드의 이름 지정
         t.Name = "TestThread";
+        // 쓰레드 뒤에서만 실행하냐 앞에서도 하냐 차이 보여주는 거라고 하심
         t.IsBackground = true;
         
         t.Start();
