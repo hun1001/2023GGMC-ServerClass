f72dc28b4a14d236fe93a56278c0917dd9b8dc2d
hun1001 <l01022899266@gmail.com>
2023-03-20 09:45:10 +0900

    thread에 대해 알아보는중

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,53 +1,26 @@
-﻿using System.Net;
-using System.Net.Sockets;
-using System.Text;
-
-namespace Client;
+﻿using System;
+using System.Threading;
 
 internal class Program
 {
+    static void MainThread()
+    {
+        for (int i = 0; i < 10; ++i)
+        {
+            Console.WriteLine("Hello thread!");
+        }
+    }
+    
+    
     private static void Main(string[] args)
     {
-        using Socket clientSocket = new(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-
-        IPEndPoint endPoint = new(IPAddress.Parse("127.0.0.1"), 5000);
-
-        bool isError = false;
-
-        try
-        {
-            clientSocket.Connect(endPoint);
-        }
-        catch
-        {
-            isError = true;
-        }
-
-        Console.WriteLine(isError ? "Connect Fail" : "Connect Succes");
-
-        while (true)
-        {
-            string str = Console.ReadLine();
-
-            if (str == "/exit")
-            {
-                return;
-            }
-
-            byte[] buffer = Encoding.UTF8.GetBytes(str);
-            clientSocket.Send(buffer);
-
-            var buffer2 = new byte[256];
-            int bytesRead = clientSocket.Receive(buffer2);
-
-            if(bytesRead < 1)
-            {
-                Console.WriteLine("Server Connect End");
-                return;
-            }
-
-            string str2 = Encoding.UTF8.GetString(buffer2);
-            Console.WriteLine($"R: {str2}");
-        }
+        Thread t = new Thread(MainThread);
+        t.Name = "TestThread";
+        t.IsBackground = true;
+        
+        t.Start();
+        
+        Console.WriteLine("Hello world!");
+        t.Join();
     }
 }
